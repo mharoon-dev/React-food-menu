84ed207498c2341e68ed4e07afa88a8f469f714a
mharoon-dev <haroon.aghani@gmail.com>
2024-02-06 23:46:25 +0500

    first component added

---
+++ src/navbar.js
@@ -0,0 +1,54 @@
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <>
+      <div style={{ position: "sticky" }} className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand ms-5 mt-3 pb-2 ps-4"
+            href="#"
+            id="logoName"
+          >
+            Foodies
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <div className="me-5" role="search">
+              <button type="button" className="btn btn-outline-secondary me-2">
+                <img
+                  style={{ verticalAlign: "text-top", marginTop: "1px" }}
+                  src="./delivery boy.png"
+                  width="25rem"
+                  alt=""
+                />
+                Delivery
+              </button>
+              <button type="button" className="btn btn-warning me-2">
+                <img
+                  style={{ verticalAlign: "text-top" }}
+                  src="./assets/waiter.png"
+                  width="30rem"
+                  alt=""
+                />
+                Call Waiter
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
